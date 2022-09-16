eb42cfe9560d17e1400311caae5d8a6a343c8a5b
Jakub Dolejs <jakubdolejs@gmail.com>
2022-09-16 13:53:03 +0100

    Redirect build script errors to stdout

--- workspace/build.sh
+++ workspace/build.sh
@@ -18,41 +18,43 @@ DEMO_SERVER_DIR=`realpath "${MY_DIR}/../demo_server"`
 echo "Cleaning"
 rm -rf "${LIBRARY_DIR}/dist" \
 "${DEMO_SERVER_DIR}/js" "${DEMO_SERVER_DIR}/demo_client" \
-"${DEMO_SERVER_DIR}/demo_client" "${DEMO_SERVER_DIR}/node_modules" "${DEMO_SERVER_DIR}/js" >/dev/null
+"${DEMO_SERVER_DIR}/demo_client" "${DEMO_SERVER_DIR}/node_modules" "${DEMO_SERVER_DIR}/js" 2>&1 >/dev/null
 
 # -------
 # Library
 # -------
 # Install dependencies
 echo "Installing library dependencies"
-cd "${LIBRARY_DIR}"
-npm install >/dev/null
+pushd "${LIBRARY_DIR}"
+npm install 2>&1 >/dev/null
 # Build
 echo "Building library"
-npx webpack >/dev/null
+npx webpack 2>&1 >/dev/null
+popd
 
 # -----------
 # Demo client
 # -----------
 # Install dependencies
-cd "${DEMO_CLIENT_DIR}"
+pushd "${DEMO_CLIENT_DIR}"
 echo "Installing demo client dependencies"
-npm install > /dev/null
+npm install 2>&1 >/dev/null
 # Build
 echo "Building demo client"
-npx tsc >/dev/null
+npx tsc 2>&1 >/dev/null
+popd
 
 # -----------
 # Demo server
 # -----------
 # Create folders for demo client source
 echo "Creating demo client source folder on demo server"
-mkdir -p "${DEMO_SERVER_DIR}/demo_client" >/dev/null
+mkdir -p "${DEMO_SERVER_DIR}/demo_client" 2>&1 >/dev/null
 # Copy demo client source
 echo "Copying demo client source to demo server"
-cp -r "${DEMO_CLIENT_DIR}/src" "${DEMO_SERVER_DIR}/demo_client" >/dev/null
+cp -r "${DEMO_CLIENT_DIR}/src" "${DEMO_SERVER_DIR}/demo_client" 2>&1 >/dev/null
 # Copy client node modules
 echo "Copying demo client Node modules to demo server"
-cp -r "${DEMO_CLIENT_DIR}/node_modules" "${DEMO_SERVER_DIR}" >/dev/null
+cp -r "${DEMO_CLIENT_DIR}/node_modules" "${DEMO_SERVER_DIR}" 2>&1 >/dev/null
 
 echo "Done"
